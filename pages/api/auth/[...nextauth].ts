5739b7f5605742e757eddb9257a75430a461f0a4
Francisco Sosa <fransosa.dev@gmail.com>
2023-04-03 02:16:51 -0300

    Notificaciones

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,7 @@
 import bcrypt from "bcrypt"
 import NextAuth from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 import prisma from "@/libs/prismadb"
 
